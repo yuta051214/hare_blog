ac205ed0179831250feec3803e4554535c5ec60e
yuta051214 <ffftrick123@gmail.com>
2022-09-25 21:31:38 +0900

    記事登録機能が完成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
+use App\Models\Post;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
 {
@@ -23,18 +27,46 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        return view('posts.create');
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Http\Requests\PostRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(PostRequest $request)
     {
-        //
+        $post = new Post($request->all());
+        $post->user_id = $request->user()->id;      // Auth()->user()->id、Auth::id() でもOK！
+
+        $file = $request->file('image');
+        $post->image = date('YmdHis').'_'.$file->getClientOriginalName();
+
+
+        // トランザクションの開始
+        DB::beginTransaction();
+        try{
+            // 画像の登録
+            $post->save();
+
+            //  画像のアップロード
+            if(!Storage::putFileAs('images/posts', $file, $post->image)){
+                // 例外を発生させてロールバックする
+                throw new \Exception('画像ファイルの保存に失敗しました。');
+            }
+
+            // トランザクションの終了
+            DB::commit();
+
+        } catch(\Exception $e) {
+            // トランザクションの失敗(終了)
+            DB::rollback();
+            return back()->withInput()->withErrors($e->getMessage());
+        }
+
+        return redirect()->route('posts.show', $post);
     }
 
     /**
@@ -45,7 +77,7 @@ class PostController extends Controller
      */
     public function show($id)
     {
-        //
+        
     }
 
     /**
@@ -62,11 +94,11 @@ class PostController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Http\Requests\PostRequest  $request
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(PostRequest $request, $id)
     {
         //
     }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,4 +13,9 @@ class Post extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    protected $fillable = [
+        'title',
+        'body',
+    ];
 }
